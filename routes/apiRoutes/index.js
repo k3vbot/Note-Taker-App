0c87d43485534be989b191615830a285b73b3dee
Kevin <muehl004@gmail.com>
2022-07-17 15:30:02 -0700

    intial commit, linked routes, added server req res

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const router = require('express').Router();
+
+fs.readFile('db/db.json','utf8', (err, data) => {
+    if (err) throw err;
+    let ntoes = JSON.parse(data);
+    
+    router.get('/', (req, res) => {
+        res.json(notes);
+    });
+
+    router.post('/', (req, res) => {
+        let newNote = req.body;
+        notes.push(newNote);
+        updateDb();
+        return console.log('Added new note: '+newNote.title);
+    });
+
+    router.get('/:id', (req, res) => {
+        res.json(notes[req.params.id]);
+    });
+
+    router.delete('/:id', (req, res) => {
+        notes.splice(req.params.id, 1);
+        updateDb();
+    });
+
+    function updateDb() {
+        fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+            if (err) throw err;
+            return true;
+        });
+    }
+});
+
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(__dirname));
+app.use(routes);
+
+app.listen(PORT, () => 
+    console.log(`Listening on port ${PORT}!`)
+);
